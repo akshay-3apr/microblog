6a3c7ae6be96d66828febd2edb4b1f7186e5273c
Akshay Prakash <prakashakshay90@gmail.com>
2019-09-12 23:17:36 +0530

    committing code changes

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 pathlibBaseDir = Path.cwd()
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'out_of_ur_range'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Secret'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FACE_ID = 'cur_face_id.jpeg'
